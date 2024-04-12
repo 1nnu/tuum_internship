b4f01cd611ce3ed8fd903df2b29c5c7f747a5047
Innar Viiname <innar.viinamae@gmail.com>
2024-04-12 15:19:29 +0300

    Initial commit

---
+++ src/test/java/ee/viinamae/tuum/TuumApplicationTests.java
@@ -0,0 +1,13 @@
+package ee.viinamae.tuum;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TuumApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
